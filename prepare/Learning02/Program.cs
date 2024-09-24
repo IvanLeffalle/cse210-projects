c80070cd83c656b00fd066df0b96c615f0580df9
IvanLeffalle <ivanmleffalle@gmail.com>
2024-09-24 09:54:41 -0300

    Abstraction Learning Activity

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+
+        job1._jobTitle = "IT Help Desk";
+        job1._company = "eAssist";
+        job1._startYear = "2023";
+        job1._endYear = "present";
+
+        Job job2 = new Job();
+
+        job2._jobTitle = "Dental Hygiene Scheduler";
+        job2._company = "Reach";
+        job2._startYear = "2023";
+        job2._endYear = "2023";
+
+        Resume myResume = new Resume();
+
+        myResume._name = "Ivan Leffalle";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+        myResume.DisplayResume();
+
     }
 }
