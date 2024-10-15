5d609116d9a45285d2678d9bef1b90118ce7fccc
IvanLeffalle <ivanmleffalle@gmail.com>
2024-10-15 19:56:24 -0300

    Last Commit

--- foundation/Foundation2/Program.cs
+++ foundation/Foundation2/Program.cs
@@ -19,6 +19,18 @@ class Program
         Console.WriteLine($"Total Price: ${newOrder.GetTotalPrice()}");
         Console.WriteLine($"Shipping Label: {newOrder.GetShippingLabel()}");
 
+        Customer newCustomer2 = new Customer("Gonzalo Leffalle", new Address("1234 Main St", "Resistencia", "Chaco", "Argentina"));
+        Product customer2Product1 = new Product("Samsung Galaxy S10", 999.99, 1, 4);
+        Product customer2Product2 = new Product("Samsung Galaxy S20", 1299.99, 1, 5);
+        Product customer2Product3 = new Product("Samsung Galaxy S30", 499.99, 2, 6);
 
+        Order newOrder2 = new Order(newCustomer2, new List<Product> { customer2Product1, customer2Product2, customer2Product3 });
+
+        Console.WriteLine("*****************************************");
+        Console.WriteLine(newOrder2.GetPackingLabel());
+        Console.WriteLine("*****************************************"); Console.WriteLine($"Products Price: ${newOrder2.GetProductsPrice()}");
+        Console.WriteLine($"Shipping Cost: ${newOrder2.GetShippingCost()}");
+        Console.WriteLine($"Total Price: ${newOrder2.GetTotalPrice()}");
+        Console.WriteLine($"Shipping Label: {newOrder2.GetShippingLabel()}");
     }
 }
--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Assignment assignment = new Assignment("Ivan Leffalle", "C#");
+        Console.WriteLine(assignment.GetSummary());
+
+        MathAssignment mathAssignment = new MathAssignment("Ivan Leffalle", "Math", "1.1", " 7-9");
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+
+        WritingAssignment writingAssignment = new WritingAssignment("Ivan Leffalle", "European History", "The Causes of World War II");
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation());
+
     }
 }
