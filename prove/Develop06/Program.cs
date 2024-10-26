109c93d4eb523107e598a3c548e70cfd3692db25
IvanLeffalle <ivanmleffalle@gmail.com>
2024-10-26 10:09:08 -0300

    comments

--- prove/Develop06/Program.cs
+++ prove/Develop06/Program.cs
@@ -1,6 +1,12 @@
 using System;
 
 class Program
+//I have added the option to add an due date so that 
+//when you create the goal you will be prompted to enter a due date. 
+//If the due date has passed then when listing your goals the 
+//program will add to that goal the label “missed” and will 
+//not allow you to record that event anymore.
+//This option only applies to simple goals
 {
     static void Main(string[] args)
     {
